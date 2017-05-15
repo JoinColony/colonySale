4e3dd58cc63f2a883176b84f9ea4256aa789d7a7
elenadimitrova <elenavdimitrova@gmail.com>
2017-05-15 11:22:09 +0300

    Test if returning correct current block number

--- test/colony-token-sale.js
+++ test/colony-token-sale.js
@@ -11,8 +11,18 @@ contract('ColonyTokenSale', function(accounts) {
     });
   });
 
-  it.skip("should not accept contributions before the start block", function() {
-  });
-  it.skip("should accept contributions after the start block", function() {
+  it("should return correct current block number", function() {
+    let currentBlockReportedByContract;
+    ColonyTokenSale.new(1234567)
+    .then(function(instance) {
+      return instance.getBlockNumber.call();
+    })
+    .then(function(currentBlock) {
+      currentBlockReportedByContract = currentBlock.toNumber();
+      return web3.eth.blockNumber;
+    })
+    .then(function(currentBlock) {
+      return assert.equal(currentBlock, currentBlockReportedByContract);
+    });
   });
 });
