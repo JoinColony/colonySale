406efa4229966aed58f1100936802c3a6914f7a5
elenadimitrova <elenavdimitrova@gmail.com>
2017-06-05 11:35:07 +0300

    Add DappSys Maths contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,9 @@
+var DSMath = artifacts.require("./DSMath.sol");
 var CLNY = artifacts.require("./CLNY.sol");
 var ColonyTokenSale = artifacts.require("./ColonyTokenSale.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(DSMath);
   deployer.deploy(CLNY);
-  deployer.link(CLNY, ColonyTokenSale);
   deployer.deploy(ColonyTokenSale);
 };
