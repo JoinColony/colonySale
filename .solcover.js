6a26a47ba4c55cee407d46c5da50a2be048a7d02
Alex Rea <git@area.io>
2017-06-27 15:28:23 +0100

    Let's get some soliditly-coverage all up in here

---
+++ .solcover.js
@@ -0,0 +1,14 @@
+module.exports = {
+    copyNodeModules: true,
+    skipFiles: [
+      'EtherRouter.sol', 
+      'dappsys/auth.sol', 
+      'dappsys/base.sol',
+      'dappsys/erc20.sol',
+      'dappsys/guard.sol',
+      'dappsys/math.sol',
+      'dappsys/note.sol',
+      'dappsys/stop.sol',
+      'dappsys/token.sol',
+    ]
+};
--- truffle.js
+++ truffle.js
@@ -12,6 +12,13 @@ module.exports = {
       port: 8545,
       network_id: 'integration',
     },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,         // <-- Use port 8555
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
+    }
   },
   mocha: {
     reporter: 'mocha-circleci-reporter',
