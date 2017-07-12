193d5c0f7d826b8747e4a8d7131df7be5145b447
elenadimitrova <elenavdimitrova@gmail.com>
2017-07-12 18:52:30 +0300

    Use static account addresses in testrpc

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -47,8 +47,13 @@ gulp.task('generate:contracts:integration', ['deploy:contracts'], async () => {
   .then(execute(`sed -ie'' s/'Pointer(destination, 0);'/'Pointer(destination, 0); pointers[stringToSig("isUpdated()")] = Pointer(destination, 32);'/g UpdatedResolver.sol`, { cwd: './contracts' }));
 });
 
-gulp.task('testrpc', () => {
-  const cmd = makeCmd(`testrpc`);
+gulp.task('testrpc', async () => {
+  const cmd = makeCmd(`
+    testrpc
+    --account="0x0355596cdb5e5242ad082c4fe3f8bbe48c9dba843fe1f99dd8272f487e70efae, 100000000000000000000"
+    --account="0xe9aebe8791ad1ebd33211687e9c53f13fe8cca53b271a6529c7d7ba05eda5ce2, 100000000000000000000"
+    --account="0x6f36842c663f5afc0ef3ac986ec62af9d09caa1bbf59a50cdb7334c9cc880e65, 100000000000000000000"
+    `);
   executeDetached(cmd);
   return waitForPort('8545');
 });
